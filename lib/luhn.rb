3380cd754d1da7b3e4357e8a825292516ff291c8
Mike Torres <apolloproductionsdj@gmail.com>
2019-08-01 16:25:15 +0000

    finished rspec testing and code for Luhn algorithm

--- lib/luhn.rb
+++ lib/luhn.rb
@@ -1,5 +1,32 @@
-module Luhn
-  def self.is_valid?(number)
-    #WRITE YOUR SOLUTION HERE
+class Luhn
+  
+  def initialize(number)
+    @number = number
   end
+
+  def is_valid?
+    @indiv_digits = individual_digits
+    @doubled = doubled_every_second_digit_from_end     
+    validate_the_sum
+  end
+
+  private
+
+  def individual_digits
+    @number.to_s.chars.map(&:to_i)
+  end
+
+  def doubled_every_second_digit_from_end
+    @indiv_digits.reverse    
+    .map.with_index { |x, i| i.odd? ? x * 2 : x } # Double every other digit
+    .map { |x| x > 9 ? x - 9 : x }  # If > 9, subtract 9 (same as adding the digits)
+    
+  end
+    
+
+  def validate_the_sum
+    @doubled.inject(:+) % 10 == 0 
+  end
+
 end
+
